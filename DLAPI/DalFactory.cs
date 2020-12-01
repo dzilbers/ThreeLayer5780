342d59cb22911b74bfb02f98e2d7d7b3eb9d0379
Dan Zilberstein <dzilbers@gmail.com>
2020-12-01 06:47:54 +0200

    Add explanation comments in DalFactory.GetDal()

--- DLAPI/DalFactory.cs
+++ DLAPI/DalFactory.cs
@@ -22,12 +22,15 @@ namespace DalApi
         /// <returns>Dal tier implementation object</returns>
         public static IDal GetDal()
         {
+            // get dal implementation name from config.xml according to <data> element
             string dalType = DalConfig.DalName;
+            // bring package name (dll file name) for the dal name (above) from the list of packages in config.xml
             string dalPackage = DalConfig.DalPackages[dalType];
+            // if package name is not found in the list - there is a problem in config.xml
             if (dalPackage == null)
                 throw new DalConfigException($"Wrong DL type: {dalType}");
 
-            try // Load concrete Dal implementation assembly
+            try // Load into CLR the dal implementation assembly according to dll file name (taken above)
             {
                 Assembly.Load(dalPackage);
             }
@@ -37,18 +40,33 @@ namespace DalApi
             }
 
             // Get concrete Dal implementation's class metadata object
+            // 1st element in the list inside the string is full class name: namespace="Dal", class name = package name
+            //    the last requirement (class name = package name) is not mandatory in general - but this is the way it
+            //    is configured per the implementation here, otherwise we'd need to add class name in addition to package
+            //    name in the config.xml file - which is clearly a good option.
+            //    NB: the class may not be public - it will still be found... Our approach that the implemntation class
+            //        should hold "internal" access permission (which is actually the default access permission)
+            // 2nd element is the package name = assembly name (as above)
             Type type = Type.GetType($"Dal.{dalPackage}, {dalPackage}");
+            // If the type is not found - the implementation is not correct - it looks like the class name is wrong...
             if (type == null)
                 throw new DalConfigException($"Class name is not the same as Assembly Name: {dalPackage}");
 
-            // Get concrete Dal implementation's Instance
+            // *** Get concrete Dal implementation's Instance
+            // Get property info for public static property named "Instance" (in the dal implementation class- taken above)
             PropertyInfo instance = type.GetProperty("Instance", BindingFlags.Public | BindingFlags.Static);
+            // If the property is not found or it's not public or not static then it is not properly implemented
+            // as a Singleton...
             if (instance == null)
                 throw new DalConfigException($"Class {dalPackage} is not a singleton");
-            IDal dal = instance.GetValue(null) as IDal; // since it's a static property - no need for an object
+            // Get the value of the property Instance (get function is automatically called by the system)
+            // Since the property is static - the object parameter is irrelevant for the GetValue() function and we can use null
+            IDal dal = instance.GetValue(null) as IDal;
+            // If the instance property is not initialized (i.e. it does not hold a real instance reference)...
             if (dal == null)
                 throw new DalConfigException($"Class {dalPackage} instance is not initialized");
 
+            // now it looks like we have appropriate dal implementation instance :-)
             return dal;
         }
     }
