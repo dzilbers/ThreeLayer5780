9cb5823d9e7bcd270df73d8f660a41b3ca1ce07d
Dan Zilberstein <dzilbers@gmail.com>
2020-12-02 18:02:07 +0200

    Add simple project architecture

---
+++ SimpleBL/BL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleDAL;
+
+namespace SimpleBL
+{
+    public class BL
+    {
+        DAL dal;
+        public BL()
+        {
+            dal = new DAL();
+        }
+
+        public override string ToString()
+        {
+            return "" + dal;
+        }
+    }
+}
---
+++ SimplePL/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleBL;
+
+namespace SimplePL
+{
+    class Program
+    {
+        static BL bl;
+        static void Main(string[] args)
+        {
+            bl = new BL();
+            Console.WriteLine(bl.ToString());
+        }
+    }
+}
